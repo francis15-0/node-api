5898c3074fa299f88befc735bf6c6cf7101096f6
francis15-0 <nnadiekwemakuo.11@gmail.com>
2025-11-29 04:16:00 +0000

    Added some changes for server functionality

--- src/server.ts
+++ src/server.ts
@@ -146,6 +146,6 @@ app.get("/data", authApi, (req: any, res) => {
   return res.send("Welcome " + user.email);
 });
 
-app.listen("3000", () => {
+app.listen("3000", '0.0.0.0', () => {
   console.log("The Server is running on port 3000");
 });
